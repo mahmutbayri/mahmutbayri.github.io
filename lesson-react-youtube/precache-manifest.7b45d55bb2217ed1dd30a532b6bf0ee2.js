5d6baab1f71f84dc17bee3ae17daa0117deb8342
Mahmut Bayri <mahmutbayri@yahoo.com>
2018-11-05 22:08:36 +0300

    Added lesson-react-youtube

---
+++ lesson-react-youtube/precache-manifest.7b45d55bb2217ed1dd30a532b6bf0ee2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2ea9b3d83f93084e7ae",
+    "url": "/lesson-react-youtube/static/js/runtime~main.b2ea9b3d.js"
+  },
+  {
+    "revision": "04553e14b826af26443f",
+    "url": "/lesson-react-youtube/static/js/main.04553e14.chunk.js"
+  },
+  {
+    "revision": "8f967b90b76d9fedf11b",
+    "url": "/lesson-react-youtube/static/js/1.8f967b90.chunk.js"
+  },
+  {
+    "revision": "c80954425725efc0a6ebba4f05dc6e8a",
+    "url": "/lesson-react-youtube/index.html"
+  }
+];
