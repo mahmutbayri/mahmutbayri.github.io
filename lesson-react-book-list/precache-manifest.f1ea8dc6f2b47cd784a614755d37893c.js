dfa11d4b3d0fc25f2a8709a1ccfc37d9aacafd9b
Mahmut Bayri <mahmutbayri@yahoo.com>
2018-11-06 14:32:38 +0300

    Update asset path

---
+++ lesson-react-book-list/precache-manifest.f1ea8dc6f2b47cd784a614755d37893c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e92c3909f5acb3ef3bbb",
+    "url": "/lesson-react-book-list/static/js/runtime~main.e92c3909.js"
+  },
+  {
+    "revision": "ffbcf6f0b466daf1cd92",
+    "url": "/lesson-react-book-list/static/js/main.ffbcf6f0.chunk.js"
+  },
+  {
+    "revision": "e35d01ff1781b9eefecd",
+    "url": "/lesson-react-book-list/static/js/1.e35d01ff.chunk.js"
+  },
+  {
+    "revision": "1c57543cd76f36baa53c504743c856fd",
+    "url": "/lesson-react-book-list/index.html"
+  }
+];
--- lesson-react-book-list/service-worker.js
+++ lesson-react-book-list/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5fc908e6be4660d6e6ff7dec3a0f8de.js"
+  "/lesson-react-book-list/precache-manifest.f1ea8dc6f2b47cd784a614755d37893c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/lesson-react-book-list/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
